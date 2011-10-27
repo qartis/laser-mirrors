fe389da7b3238e4f3789d3c0a1873708a18ef6e4
Andrew Fuller <qartis@gmail.com>
2011-10-27 01:33:13 -0700

    proper window title

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include <FL/Fl_Double_Window.H>
 #include <FL/fl_draw.H>
 
-#include "gui.h"
+#include "grid.h"
 
 int main(int argc, char **argv) {
-    grid g(0, 0, 7*60, 7*60, "hey");
+    grid g(0, 0, 7*60, 7*60, "laser mirrors");
     g.init();
 
     g.show(argc,argv);
